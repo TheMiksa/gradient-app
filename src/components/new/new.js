30180e06e6662af858318c34af8316e5cbc12310
TheMiksa <michael.gritskov@gmail.com>
2021-02-17 18:44:58 +0200

    added creating gradient functional

--- src/components/new/new.js
+++ src/components/new/new.js
@@ -1,17 +1,39 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom"
+import {v4} from "uuid";
 import "./new.css";
 
 export const New = () => {
     const [firstColor, setFirstColor] = useState("fff");
     const [secondColor, setSecondColor] = useState("000");
+    const [valid, toggleValid] = useState(true);
 
     const toCheckHexColors = (value, setColor) => {
-        if (value.length === 3 || value.length === 6) setColor(value);
-    }
+        if (value.length === 3 || value.length === 6) {
+            setColor(value);
+            !valid && toggleValid(true);
+        }
+        else valid && toggleValid(false);
+    };
+    const toAddGradient = (first, second) => {
+        const gradientsList = JSON.parse(localStorage.getItem("gradientsList"));
+        const newGradient = {
+          id: v4(),
+          firstColor: "#" + firstColor,
+          secondColor: "#" + secondColor
+        };
+
+        gradientsList.push(newGradient);
+        localStorage.setItem("gradientsList", JSON.stringify(gradientsList));
+    };
+    const btn = valid ? (
+        <Link to="/" className="btn btn-gradient" onClick={() => toAddGradient(firstColor, secondColor)}>Add Gradient</Link>
+        ) : (
+        <button className="btn btn-danger" disabled={true}>Invalid values</button>
+        );
     return (
         <div>
-            <Link to="/" className="btn btn-gradient">back to home</Link>
+            {btn}
             <br/>
             <input type="text" onChange={({target}) => toCheckHexColors(target.value, setFirstColor)}/>
             <input type="text" onChange={({target}) => toCheckHexColors(target.value, setSecondColor)}/>
