7b2d2ed5f4386a15e2f6859b244b52562942eeff
TheMiksa <michael.gritskov@gmail.com>
2021-02-21 13:34:03 +0200

    text color logic has changed

--- src/components/gradient/gradient.js
+++ src/components/gradient/gradient.js
@@ -6,7 +6,7 @@ export const Gradient = ({id, firstColor, secondColor, onDeleteGradient}) => {
         if (color.length === 4) {
             return color.match(/[a-fA-F]{2}/) ? "black" : "white";
         } else if (color.length === 7) {
-            return color.match(/[a-fA-F]\w[a-fA-F]\w[a-fA-F]\w/) ? "black" : "white";
+            return color.match(/([a-fA-F]\w){2}/) ? "black" : "white";
         }
     };
     return (
